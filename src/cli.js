66c2158d0a4f47a96e883db99a5e52259cd174b3
Aleksey Kostin <alekkostin91@gmail.com>
2022-02-13 03:15:30 +0300

    Added cli.js

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+const requestName = (quest) => {
+    const userName = readlineSync.question(`${String(quest)} `);
+    console.log(`Hello, ${userName}!`); 
+};
+export default requestName;
