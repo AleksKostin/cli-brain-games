5eda068d87f3bdbe2406cfba917f73559dcbef98
Aleksey Kostin <alekkostin91@gmail.com>
2022-02-19 17:19:39 +0300

    Add directory games

---
+++ games/game-calc.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import {
+  randomElement, greeting, wrongAnswer, gameQuestion,
+} from '../src/index.js';
+
+const brainCalc = () => {
+  const userName = greeting();
+  console.log('What is the result of the expression?');
+  const operator = ['+', '-', '*'];
+  let i = 0;
+  let randomOperator;
+  let expression;
+  let answer;
+  let num1;
+  let num2;
+  while (i < 3) {
+    randomOperator = operator[randomElement(operator.length)];
+    expression = `${num1 = randomElement(10)} ${randomOperator} ${num2 = randomElement(10)}`;
+    const query = gameQuestion(expression);
+    switch (randomOperator) {
+      case '+':
+        answer = num1 + num2;
+        break;
+      case '-':
+        answer = num1 - num2;
+        break;
+      default:
+        answer = num1 * num2;
+        break;
+    }
+    if (query === String(answer)) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      return wrongAnswer(query, answer, userName);
+    }
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};
+
+export default brainCalc;
