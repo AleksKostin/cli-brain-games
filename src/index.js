1150e08cc3648e73977c0da26a214d8f2d53835c
Aleksey Kostin <alekkostin91@gmail.com>
2022-02-19 17:16:54 +0300

    Added index.js with common logic for all games

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+const randomElement = (element) => {
+  const tmp = element;
+  const random = Math.floor(Math.random() * tmp);
+  return random;
+};
+
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
+};
+
+const wrongAnswer = (wrong, correct, userName) => {
+  const wrongCorrect = `'${wrong}' is wrong answer ;(. Correct answer was '${correct}'.\nLet's try again, ${userName}!`;
+  console.log(wrongCorrect);
+};
+
+const gameQuestion = (value) => {
+  const query = readlineSync.question(`Question: ${value}\nYour answer: `);
+  return query;
+};
+
+export {
+  randomElement, greeting, wrongAnswer, gameQuestion,
+};
